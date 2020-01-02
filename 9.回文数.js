b82fd54aeb8c658a0daadd05704f967633021047
Reamd7 <reamd7@gmail.com>
2020-01-02 15:57:57 +0800

    update

--- 9.回文数.js
+++ 9.回文数.js
@@ -10,15 +10,24 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) return false;
-    let source = x;
+    // x < 0 => false
+    // 尾数 0 且 不为 0 => false
+    if (x == 0) return true;
+    if (x < 0 || x % 10 == 0) return false;
+    // let source = x;
     let rev = 0;
-    while (x !== 0){
+
+    // 若 x > rev
+    while (x > rev){
+        // 从右向左 popX 迭代
         let popX = x % 10;
         x = (x - popX) / 10;
+
         rev = rev * 10 + popX;
     };
-    return rev === source;
+    // 1221 12 === 21 偶位数
+    // 12321 12 === (123 - 123 % 10) / 10 奇位数，整除，丢弃3
+    return x === rev || x === (rev - rev % 10) / 10;
 };
 // @lc code=end
 
